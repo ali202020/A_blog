cd047f03acb58e9f741b2cb5a3e09a3d9d40159b
ali202020 <az_alaseel2010@yahoo.com>
2018-11-04 00:02:35 +0200

    finished NewComment Event implementation

--- app/Events/NewComment.php
+++ app/Events/NewComment.php
@@ -9,19 +9,24 @@ use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use App\Comment;
 
-class NewComment
+class NewComment implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+    private $comment;
 
     /**
      * Create a new event instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Comment $comment)
     {
-        //
+        $this->comment = $comment;
     }
 
     /**
@@ -31,6 +36,6 @@ class NewComment
      */
     public function broadcastOn()
     {
-        return new PrivateChannel('channel-name');
+        return new Channel('post.'.$this->comment->post->id);
     }
 }
