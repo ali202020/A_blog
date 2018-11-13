b8beb957b04861ee093b1de6cc6ff1d9d3171d18
ali202020 <az_alaseel2010@yahoo.com>
2018-11-13 17:59:03 +0200

    initial (Footer,Advertisement) widget implementation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -104,6 +104,7 @@
 
         {{-- ***  Start Of Create Post Widget ***--}}
         <div class="column is-3 p-l-5">
+          {{-- Create post Button --}}
           <div class="tile posts-container is-customized-tile post-widget-container box ">
 
             @if (Auth::check())
@@ -112,6 +113,49 @@
               <a class="button is-primary is-half" title="Please Login To Create A Post" disabled>Create New Post</a>
             @endif
           </div>
+          {{-- ***************** --}}
+
+          {{-- Advertisements Section --}}
+          <div class="tile box columns ad-widget">
+            <div class="column is-12">
+              <p class="ad-title">Advertisement</p>
+              <hr>
+              <figure class="image is-4by5">
+                <img src="https://bulma.io/images/placeholders/256x256.png">
+              </figure>
+            </div>
+          </div>
+          {{-- ************************--}}
+
+          {{-- Website Footer section --}}
+          <div class="tile box columns is-multiline footer-widget">
+            <div class="column is-6">
+              <ul>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Press</a></li>
+              </ul>
+            </div>
+
+            <div class="column is-6">
+              <ul>
+                <li><a href="#">Advertise</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Help</a></li>
+              </ul>
+            </div>
+
+            <div class="column is-12" style="text-align:center;">
+              <ul>
+                <li><span><a href="#">Content Policy</a></span> | <span><a href="#">Privacy Policy</a></span></li>
+                <li><span><a href="#">User Agreement</a></span> | <span><a href="#">Mod Policy</a></span></li>
+                <li><i class="fa fa-copyright fa-fw"></i> <span>2018 A_BLOG, Inc. All rights reserved</span></li>
+              </ul>
+            </div>
+          </div>
+          {{-- *********************** --}}
+
+
         </div>
       {{-- ***  End Of Create Post Widget ***--}}
       </div>
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('auth.register');
-});
+Route::get('/', 'HomeController@index');
 
 Auth::routes();
 
